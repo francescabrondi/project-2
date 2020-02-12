4e21b83a660904d38582fc340a9d3ba57587d76e
Francesca <francescabrondi@gmail.com>
2020-02-12 10:36:04 +0100

    Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Recipe from "./components/Recipe";
+import Suggestions from "./components/Suggestions";
+import Footer from "./components/Footer";
+import Icons from "./components/Icons";
+
+import "./scss/styles.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
