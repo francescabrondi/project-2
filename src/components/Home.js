f12afab2599d5e2dba221077d9d0de362ce56fad
emi.luciani18@gmail.com <59806361+emiluce@users.noreply.github.com>
2020-02-25 14:52:56 +0100

    commit

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Home = () => (
+  <div class="card">
+    <main role="main" />
+    <div class="box">
+      <div class="overlay">
+        <div class="titles">
+          <div class="imagetext">
+            So, what do you want to eat?
+          </div>
+          <div class="text">
+            Start by selecting your ingredients!
+          </div>
+        </div>
+        <div class="scroll">
+          Scroll down
+        </div>
+        <i class="Chevron-compact-down">Icon</i>
+      </div>
+    </div>
+  </div>
+);
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Navbar = () => (
+  <div className="nav">
+    <nav className="navbar navbar-expand-lg">
+      <a className="brand" href="#">FOODHUB</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <i class="search">Icon</i>
+    </nav>
+  </div>
+);
+
+export default Navbar;
